0f2dbc290e1269b84716c9c21406f70d7395db15
Josphat84 <151152963+Josphat84@users.noreply.github.com>
2025-07-23 02:25:58 +0200

    Fixed build configuration

--- frontend/app/privacy-policy/page.tsx
+++ frontend/app/privacy-policy/page.tsx
@@ -1,6 +1,6 @@
 // frontend/app/privacy-policy/page.t
 import Header from '@/components/Header';
-i//mport Footer from '@/components/Footer';
+import Footer from '@/components/Footer';
 import next from 'next';
 
 export default function PrivacyPolicyPage() {
